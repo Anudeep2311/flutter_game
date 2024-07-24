02a285db554fc0c1faefb9cd51b65d47a45dba18
Anudeep2311 <anudeeplohghavkar2311@gmail.com>
2024-07-24 19:58:18 +0530

    First Commit

---
+++ lib/pixle_adventure.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_game/levels/level.dart';
+
+class PixelAdventure extends FlameGame {
+  @override
+  Color backgroundColor() => const Color(0xFF211F30);
+  late final CameraComponent cam;
+  final world = Level(levelName: 'Level-02');
+  @override
+  FutureOr<void> onLoad() async {
+    // Load all images into cache
+    await images.loadAllImages();
+
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 640, height: 360);
+    cam.viewfinder.anchor = Anchor.topLeft;
+
+    addAll([cam, world]);
+
+    return super.onLoad();
+  }
+}
