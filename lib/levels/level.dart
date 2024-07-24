a2b59c967bde4fa7c11751f402b53c477f5c54cd
Anudeep2311 <anudeeplohghavkar2311@gmail.com>
2024-07-24 22:57:37 +0530

    Joystick Added

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -6,7 +6,11 @@ import 'package:flutter_game/actors/player.dart';
 
 class Level extends World {
   final String levelName;
-  Level({required this.levelName});
+  final Player player;
+  Level({
+    required this.levelName,
+    required this.player,
+  });
   late TiledComponent level;
   @override
   FutureOr<void> onLoad() async {
@@ -20,10 +24,7 @@ class Level extends World {
     for (final spawnPoint in spawnPointsLayer!.objects) {
       switch (spawnPoint.class_) {
         case 'Player':
-          final player = Player(
-            character: 'Ninja Frog',
-            position: Vector2(spawnPoint.x, spawnPoint.y),
-          );
+          player.position = Vector2(spawnPoint.x, spawnPoint.y);
           add(player);
           break;
 
--- lib/pixle_adventure.dart
+++ lib/pixle_adventure.dart
@@ -1,26 +1,59 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_game/actors/player.dart';
 import 'package:flutter_game/levels/level.dart';
 
-class PixelAdventure extends FlameGame {
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
   late final CameraComponent cam;
-  final world = Level(levelName: 'Level-02');
+  Player player = Player(character: 'Mask Dude');
+  late JoystickComponent joyStick;
   @override
   FutureOr<void> onLoad() async {
     // Load all images into cache
     await images.loadAllImages();
 
+    final world = Level(
+      levelName: 'Level-01',
+      player: player,
+    );
     cam = CameraComponent.withFixedResolution(
-        world: world, width: 640, height: 360);
+      world: world,
+      width: 640,
+      height: 360,
+    )
+      ..priority = 0
+      ..viewfinder.anchor = Anchor.bottomLeft;
     cam.viewfinder.anchor = Anchor.topLeft;
 
     addAll([cam, world]);
 
+    addJoyStick();
+
     return super.onLoad();
+  }
+
+  void addJoyStick() {
+    joyStick = JoystickComponent(
+      priority: 1,
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Joystick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+    add(joyStick);
   }
 }
